6fd72d6769eb67db0afb939b19b3bd1cc620be64
Jose Orea <jose.orea.15@gmail.com>
2019-12-02 19:58:02 -0500

    JSX complete

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -4,8 +4,27 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 // create a react component
+// function getButtonText() {
+//   return "Click on Me!";
+// }
 const App = () => {
-  return <div>Hi there!</div>;
+  // const buttonText = "Click Me!";
+
+  const textError = { text: "click me" };
+  const style = { backgroundColor: "blue", color: "white" };
+  return (
+    <div>
+      <label className="name" htmlFor="name">
+        Enter Name
+      </label>
+      <input id="name" type="text"></input>
+      <button style={style}>
+        {/* {textError} */}
+        {/* produces error */}
+        {textError.text}
+      </button>
+    </div>
+  );
 };
 // Take the react component and show it on the screen
 
